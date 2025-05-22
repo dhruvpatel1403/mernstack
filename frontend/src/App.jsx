6b4626e5f2d2326fd6b0cfcd67bb0e04c9f1650e
Dhruv Patel <dhruvkumarpatel1403@gmail.com>
2025-05-22 16:31:30 -0300

    Created hello world app

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <p><h1>Hello World</h1></p>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
